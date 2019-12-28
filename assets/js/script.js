333ab9d504e64e6a4a734488deabd45d9616cccf
Mustafa Dalğa <mustafadalgaa@gmail.com>
2019-12-28 21:17:51 +0300

    Completed Navbar Section

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,18 @@
+$(document).ready(function () {
+
+  $(".nav-button").click(function () {
+    $(this).toggleClass("change");
+  });
 
 
+  $(window).scroll(function () {
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    let position = $(this).scrollTop();
+    var a = $(this).scrollTop();
+    if (position >= 200) {
+      $(".nav-menu").addClass("custom-navbar");
+    } else {
+      $(".nav-menu").removeClass("custom-navbar");
+    }
+  });
+});
