b6c7bfb311d5979da1f96ad4ad9c9ae3a632f4d8
Mustafa Dalğa <mustafadalgaa@gmail.com>
2019-12-29 15:25:01 +0300

    Adding scroolSpy, animate scrool and dynamic footer year

--- assets/js/script.js
+++ assets/js/script.js
@@ -63,6 +63,27 @@ $(document).ready(function () {
 
   });
 
+  $('#year').text(new Date().getFullYear());
+
+  $('body').scrollspy({ target: '#main-nav' });
+
+  $('#main-nav a').on('click', function (event) {
+    if (this.hash !== '') {
+      event.preventDefault();
+
+      const hash = this.hash;
+
+      $('html, body').animate(
+        {
+          scrollTop: $(hash).offset().top
+        },
+        800,
+        function () {
+          window.location.hash = hash;
+        }
+      );
+    }
+  });
 
 
 });
