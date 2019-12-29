a8da2d70be3553543654b384917e4cf8d0280db5
Mustafa Dalğa <mustafadalgaa@gmail.com>
2019-12-29 15:39:56 +0300

    Unnecessary codes deleted and order of functions corrected.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,10 @@
 $(document).ready(function () {
 
+  // Navbar Section Mobile Hamburger Menu
   $(".nav-button").click(function () {
     $(this).toggleClass("change");
   });
 
-
-  // Navbar Section
   $(window).scroll(function () {
 
     let position = $(this).scrollTop();
@@ -15,13 +14,8 @@ $(document).ready(function () {
     } else {
       $(".nav-menu").removeClass("custom-navbar");
     }
-  });
 
-
-  // Mission Section
-  $(window).scroll(function () {
-
-    var position = $(this).scrollTop();
+    // Mission Section Effects
     if (position >= 650) {
       $('.camera-img').addClass("from-left");
       $(".mission-text").addClass("from-right");
@@ -29,6 +23,18 @@ $(document).ready(function () {
       $('.camera-img').removeClass("from-left");
       $(".mission-text").removeClass("from-right");
     }
+
+    // Pricing Section Effects
+    if (position >= 4300) {
+      $(".card-1").addClass("move-from-left");
+      $(".card-2").addClass("move-from-bottom");
+      $(".card-3").addClass("move-from-right");
+    } else {
+      $(".card-1").removeClass("move-from-left");
+      $(".card-2").removeClass("move-from-bottom");
+      $(".card-3").removeClass("move-from-right");
+    }
+
   });
 
 
@@ -47,26 +53,14 @@ $(document).ready(function () {
   });
 
 
-  // Pricing Section
-  $(window).scroll(function () {
 
-    let position = $(this).scrollTop();
-    if (position >= 4300) {
-      $(".card-1").addClass("move-from-left");
-      $(".card-2").addClass("move-from-bottom");
-      $(".card-3").addClass("move-from-right");
-    } else {
-      $(".card-1").removeClass("move-from-left");
-      $(".card-2").removeClass("move-from-bottom");
-      $(".card-3").removeClass("move-from-right");
-    }
-
-  });
-
+  // Dynamic Footer Year
   $('#year').text(new Date().getFullYear());
 
+  // Init Scroolspy
   $('body').scrollspy({ target: '#main-nav' });
 
+  //Animate Scrool
   $('#main-nav a').on('click', function (event) {
     if (this.hash !== '') {
       event.preventDefault();
